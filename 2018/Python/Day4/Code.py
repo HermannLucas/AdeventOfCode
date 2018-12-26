0d9eda603af1a3e7869c8dba20f0570f138cc690
Lucas Hermann <herm.lucas@gmail.com>
2018-12-26 21:11:10 +0000

    Added function to find the id of the guard who spent the longest assleep

--- 2018/Python/Day4/Code.py
+++ 2018/Python/Day4/Code.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime
+import datetime
 
 class Record(object):
 
@@ -16,7 +16,9 @@ class Record(object):
         return cls("", [change])
 
 class day4(object):
-    sleep_table = {}
+
+    def __init__(self):
+        self.sleep_table = {}
 
     def read_records(self, records):
         for record in records.split(", "):
@@ -24,7 +26,7 @@ class day4(object):
             yield self.read_time_stamp(match.group(1)), match.group(2)
 
     def read_time_stamp(self, time_stamp):
-        return datetime.strptime(time_stamp, '%Y-%m-%d %H:%M')
+        return datetime.datetime.strptime(time_stamp, '%Y-%m-%d %H:%M')
 
     def read_guard_id(self, text):
         return int(re.match(r'Guard #(.*) begins shift', text).group(1))
@@ -38,6 +40,8 @@ class day4(object):
                 Record.with_change(int(time_stamp.strftime('%M')))
 
     def add_guard_id(self, time_stamp, guard_id):
+        if time_stamp.strftime('%H') == "23":
+            time_stamp += datetime.timedelta(days=1)
         try:
             self.sleep_table[time_stamp.strftime('%m-%d')]\
                 .guard_id = guard_id
@@ -54,3 +58,14 @@ class day4(object):
 
         for day in self.sleep_table.values():
             day.changes.sort()
+
+    def find_sleeper(self):
+        sleep_time = {}
+        for day in self.sleep_table.values():
+            try:
+                sleep_time[day.guard_id] += sum([sleep[1] - sleep[0] for sleep \
+                    in (zip(day.changes[::2], day.changes[1::2]))])
+            except:
+                sleep_time[day.guard_id] = sum([sleep[1] - sleep[0] for sleep \
+                    in (zip(day.changes[::2], day.changes[1::2]))])
+        return max(sleep_time, key= sleep_time.get)
--- 2018/Python/Day4/Test.py
+++ 2018/Python/Day4/Test.py
@@ -44,6 +44,13 @@ class TestDay1(unittest.TestCase):
         self.assertEqual([*self.sut.sleep_table], expected_result_keys)
         self.assertEqual(self.sut.sleep_table["11-01"].changes, expected_result_changes)
 
+    def test_find_sleeper(self):
+
+        problem = "[1518-11-01 00:00] Guard #10 begins shift, [1518-11-01 00:05] falls asleep, [1518-11-01 00:25] wakes up, [1518-11-01 00:30] falls asleep, [1518-11-01 00:55] wakes up, [1518-11-01 23:58] Guard #99 begins shift, [1518-11-02 00:40] falls asleep, [1518-11-02 00:50] wakes up, [1518-11-03 00:05] Guard #10 begins shift, [1518-11-03 00:24] falls asleep, [1518-11-03 00:29] wakes up, [1518-11-04 00:02] Guard #99 begins shift, [1518-11-04 00:36] falls asleep, [1518-11-04 00:46] wakes up, [1518-11-05 00:03] Guard #99 begins shift, [1518-11-05 00:45] falls asleep, [1518-11-05 00:55] wakes up"
+        expected_result = 10
+
+        self.sut.fill_sleep_table(problem)
+        self.assertEqual(self.sut.find_sleeper(), expected_result)
 
 if __name__ == '__main__':
     unittest.main()
