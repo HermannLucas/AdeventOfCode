1747cd1b3b713dc4ecf79b1a91ded6a920b1ebbd
Lucas Hermann <herm.lucas@gmail.com>
2018-12-30 22:06:04 +0000

    Started Day7 problem2
    Got example to work but get an answer too low for problem 2

--- 2018/Python/Day7/Code.py
+++ 2018/Python/Day7/Code.py
@@ -1,10 +1,54 @@
 import re
+from collections import deque
+
+class Worker(object):
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.instruction = None
+
+    def update(self):
+        if self.instruction:
+            self.instruction.update()
+        else:
+            if len(self.parent.ready_queue) > 0:
+                self.add_instruction(self.parent.next_instruction())
+                try:
+                    self.instruction.update()
+                except:
+                    pass
+
+    def add_instruction(self, name):
+        self.instruction = Instruction(name, self)
+
+    def done_instruction(self, name):
+        self.instruction = None
+        self.parent.work_done(name)
+
+class Instruction(object):
+
+    def __init__(self, name, parent):
+        self.name = name
+        self.time = 60 + ord(name) %64
+        self.parent = parent
+
+    def work(self):
+        self.time -= 1
+
+    def finished(self):
+        self.parent.done_instruction(self.name)
+
+    def update(self):
+        self.work()
+        if self.time == 0:
+            self.finished()
 
 class day6(object):
 
     def __init__(self):
         self.steps = set()
         self.prerequisites = {}
+        self.dependants = {}
 
     def read_instructions(self, instructions):
         for instruction in instructions.split(", "):
@@ -37,3 +81,48 @@ class day6(object):
                     self.steps.remove(step)
                     break
         return "".join(step_list)
+
+    def fill_dependants(self, instructions):
+        for action, next in self.read_instructions(instructions):
+            self.steps.add(action)
+            self.steps.add(next)
+            try:
+                self.dependants[action].append(next)
+            except:
+                self.dependants[action] = [next]
+
+    def problem2(self, instructions, workers):
+        self.fill_prerequisites(instructions)
+        instruction_count = len(self.steps)
+        time = 1
+        self.ready_queue = deque([step for step in self.steps if step not in self.prerequisites.keys()])
+        self.done_list = []
+        self.steps = sorted(list(self.steps))
+        workers = [Worker(self) for _ in range(workers)]
+        while len(self.done_list) < instruction_count:
+            time += 1
+            for worker in workers:
+                worker.update()
+        return time
+
+    def work_done(self, instruction):
+        self.done_list.append(instruction)
+        try:
+            del self.prerequisites[instruction]
+        except:
+            pass
+        finally:
+            self.update_ready_queue()
+
+    def next_instruction(self):
+        try:
+            return self.ready_queue.popleft()
+        except:
+            return None
+
+    def update_ready_queue(self):
+        for instruction in self.prerequisites.keys():
+            for prerequisite in self.prerequisites[instruction]:
+                if prerequisite not in self.done_list:
+                    continue
+            self.ready_queue.append(instruction)
