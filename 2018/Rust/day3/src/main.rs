117723f1a9dd3f0f98d65e28074b0d932a3f075e
Lucas Hermann <herm.lucas@gmail.com>
2019-02-22 00:05:33 +0000

    Solved Day 3 Problem1
    Solved Day 3 Problem1 in Rust
    solution based on a mixture of Adam's solution (Scala) and my Python3 solution

---
+++ 2018/Rust/day3/src/main.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+const PUZZLE: &str = include_str!("input.txt");
+
+#[derive(Hash, Eq, PartialEq)]
+struct Cell {
+    x: u16,
+    y: u16,
+}
+
+struct Canvas {
+    cells: HashMap<Cell, Vec<u16>>
+}
+
+impl Canvas {
+    fn add_claim(&mut self, claim: Claim) {
+        for y in claim.start.y..claim.end.y {
+            for x in claim.start.x..claim.end.x {
+                let ids = self.cells.entry(Cell {x: x, y: y}).or_insert(Vec::new());
+                ids.push(claim.id);
+            }
+        }
+    }
+}
+
+struct Claim {
+    id: u16,
+    start: Cell,
+    end: Cell
+}
+
+impl Claim {
+    fn create(line: Vec<u16>) -> Claim {
+        Claim {
+            id: line[0],
+            start: Cell{x: line[1], y: line[2]},
+            end: Cell{x: line[1] + line[3], y: line[2] + line[4]}
+        }
+    }
+}
+
+fn main() {
+    let mut fabric =  Canvas {cells: HashMap::new()};
+    for line in PUZZLE.lines() {
+        fabric.add_claim(Claim::create(line.split(|c|
+            c == ' ' ||
+            c == '@' ||
+            c == ',' ||
+            c == ':' ||
+            c == 'x' ||
+            c == '#')
+            .filter_map(|c| c.parse::<u16>().ok()).collect::<Vec<_>>()));
+    }
+    let result = fabric.cells.values().filter(|c| c.len() > 1).count();
+    assert_eq!(108961, result);
+    println!("{}", result);
+}
