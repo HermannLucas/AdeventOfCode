e9032fc1b67a67bbfc41ddb1f0b580270066d573
LucasH <herm.lucas@gmail.com>
2019-12-02 23:16:50 +0000

    Solve part 2 day 1

--- 2019/Rust/day1/src/main.rs
+++ 2019/Rust/day1/src/main.rs
@@ -1,5 +1,13 @@
 const PUZZLE: &str = include_str!("../input.txt");
 
+fn part2(mut input: usize) -> usize {
+    let mut result = Vec::new();
+    while input >= 6 {
+        input = input.div_euclid(3) - 2;
+        result.push(input);
+    }
+    result.iter().sum()
+}
 fn main() {
     let result: usize = PUZZLE
         .lines()
@@ -7,4 +15,11 @@ fn main() {
         .map(|m| m.div_euclid(3) - 2)
         .sum();
     println!("One: {:?}", result);
+
+    let result: usize = PUZZLE
+        .lines()
+        .map(|i| i.parse::<usize>().unwrap())
+        .map(|m| part2(m))
+        .sum();
+    println!("Two: {:?}", result);
 }
